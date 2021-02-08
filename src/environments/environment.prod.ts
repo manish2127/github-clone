fea8c0cba42ecce5311197064d4f5fe7d9f8b747
Manish Jain <68476832+manish2127@users.noreply.github.com>
2021-02-08 12:05:50 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB7TsIk48tGTWyPFltMjKaNTsk7vb6brUk",
+    authDomain: "dummy-ad1fe.firebaseapp.com",
+    projectId: "dummy-ad1fe",
+    storageBucket: "dummy-ad1fe.appspot.com",
+    messagingSenderId: "992017774219",
+    appId: "1:992017774219:web:e1057ec49cf28f4c55068e"
+  },
 };
